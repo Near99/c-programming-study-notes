537c28ee3202f50c63b25d82e9365c996dc87bc9
Charles <wkx0823@gmail.com>
2022-09-12 19:31:28 +0800

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+// Pointer values are strongly typed.
+int pointer_basics();
+int pointer_to_pointer();
+int pointer_as_function_arguments();
+
+int main()
+{
+    // pointer_basics();
+
+    pointer_to_pointer();
+
+    pointer_as_function_arguments();
+}
+
+int pointer_basics()
+{
+    int a = 1025;
+    int *p = &a;
+
+    // Address of pointer p.
+    printf("%p\n", &p);
+
+    // Address of where p is pointing to which is a's address.
+    printf("%p\n", p);
+
+    // Address of a which is equal to p's value.
+    printf("%p\n", &a);
+
+    // Value of a, the operation is called dereferencing.
+    printf("%d\n", *p);
+
+    char *pc;
+
+    // typecasting
+    pc = (char *)p;
+    printf("address = %p, value = %d\n", pc, *pc);
+    // *pc + 1 means dereference the value of pointer pc which is 1 then incrementing by 1
+    // *(pc + 1) means to increment the address by 1 then dereferencing.
+    printf("address = %p, value = %d\n", pc + 1, *(pc + 1));
+
+    // Generic pointer.
+    void *pv;
+
+    return 0;
+}
+
+int pointer_to_pointer()
+{
+    int x = 5;
+    int *p = &x;
+    int **q = &p;
+    int ***r = &q;
+
+    // address of x.
+    printf("%p\n", &x);
+    // p's value is the address of x.
+    printf("%p\n", p);
+    // dereferencing a pointer to a pointer, value is the address of where p is pointing to which is x's address.
+    printf("%p\n", *q);
+
+    printf("Address of x is: %p, value of X is: %d\n", &x, x);
+    printf("Address of P is: %p, value of P is: %p\n", &p, p);
+    printf("Address of Q is: %p, value of Q is: %p\n", &q, q);
+    printf("Address of R is: %p, value of R is: %p\n", &r, r);
+
+    printf("%d\n", *p);
+    **q = *p + 2;
+    printf("%d\n", *(*q));
+    ***r = **q + 2;
+    printf("%d\n", *(*(*r)));
+}
+
+int pointer_as_function_arguments()
+{
+}
+// Tokyo - lofi geek
+// Dew - Casiio, Sleepermane
